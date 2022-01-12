22b81c2eaf25bb2a311744b8eae8fa01c544fc3a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-12 07:53:21 -0300

    Feat: implement sorting of Strings with new ComparadorPorTamanho class, inside OrdenaString

--- src/com.ndrewcoding/OrdenaString.java
+++ src/com.ndrewcoding/OrdenaString.java
@@ -1,4 +1,29 @@
 package com.ndrewcoding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class OrdenaString {
+    public static void main(String[] args) {
+
+        List<String> palavras = new ArrayList<>();
+        palavras.add("Alura");
+        palavras.add("Portfólio");
+        palavras.add("Ajax");
+
+        palavras.sort(new ComparadorPorTamanho());
+
+        System.out.println(palavras);
+
+    }
+}
+
+class ComparadorPorTamanho implements Comparator<String> {
+
+    @Override
+    public int compare(String s1, String s2) {
+        return s1.compareTo(s2);
+    }
+
 }
