a08df6073d4ac2553969a156bf4fc0df2eb2e0fc
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-12 21:45:19 -0300

    Chore: create class Curso inside ExemploCursos

--- src/com.ndrewcoding/ExemploCursos.java
+++ src/com.ndrewcoding/ExemploCursos.java
@@ -1,4 +1,28 @@
 package com.ndrewcoding;
 
+class Curso {
+
+    private String nome;
+    private int quantidadeDeAlunos;
+
+    public Curso(String nome, int quantidadeDeAlunos) {
+        this.nome = nome;
+        this.quantidadeDeAlunos = quantidadeDeAlunos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidadeDeAlunos() {
+        return quantidadeDeAlunos;
+    }
+
+}
+
 public class ExemploCursos {
+    public static void main(String[] args) {
+
+
+    }
 }
