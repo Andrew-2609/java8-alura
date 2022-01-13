58e7d5e91eedfa4713e3d2f06128b5d3758c5eef
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-12 21:49:05 -0300

    Feat: implement main() method of ExemploCursos

--- src/com.ndrewcoding/ExemploCursos.java
+++ src/com.ndrewcoding/ExemploCursos.java
@@ -1,5 +1,9 @@
 package com.ndrewcoding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 class Curso {
 
     private String nome;
@@ -18,11 +22,24 @@ class Curso {
         return quantidadeDeAlunos;
     }
 
+    @Override
+    public String toString() {
+        return "Curso(nome: " + nome + ", quantidade de alunos: " + quantidadeDeAlunos + ")";
+    }
 }
 
 public class ExemploCursos {
     public static void main(String[] args) {
 
+        List<Curso> cursos = new ArrayList<>();
+        cursos.add(new Curso("Python", 45));
+        cursos.add(new Curso("Java", 80));
+        cursos.add(new Curso("JavaScript", 70));
+        cursos.add(new Curso("Ruby", 30));
+
+        cursos.sort(Comparator.comparing(Curso::getQuantidadeDeAlunos));
+
+        cursos.forEach(System.out::println);
 
     }
 }
